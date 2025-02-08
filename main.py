684470699d4d3b4ecb4c5a3a39c0b2b00c58b200
Jannis Kaniaros <kanjannis@gmail.com>
2025-02-08 21:49:23 +0100

    Multigoal

--- main.py
+++ main.py
@@ -1,81 +1,71 @@
 import pyoptinterface as poi
+import numpy as np
 from pyoptinterface import highs
 
+from enums.customer_agent_state import CustomerAgentState
+from mesa_objects.agents.customer_agent import CustomerAgent
+from mesa_objects.agents.manager_agent import ManagerAgent
+from mesa_objects.agents.service_agent import ServiceAgent
 from mesa_objects.models.restaurant_model import RestaurantModel
 from models.config.config import Config
 
 
-def model_run(restaurant: RestaurantModel, service_agents: int) -> float:
+def objective_function(x) -> tuple[int, float]:
     """
     Objective function to minimize the total waiting time in the restaurant
     :param service_agents: Number of service agents
     """
-    # Set the number of service agents and parallel preparation according to the optimization
-    Config().service.service_agents = service_agents
-
-    # Run the model with the updated configuration
-    while restaurant.running and restaurant.steps < Config().run.step_amount:
-        restaurant.step()
-
-    sum_real_waiting_time = restaurant.get_total_waiting_time()
-    sum_ideal_waiting_time = restaurant.get_total_ideal_time()
-
-    # Difference is the delay - should always be positive because the idel time does not contain the delay for larger groups. Real >! Ideal
-    # The objective is to minimize the delay
-    
-    # TODO: Add other optimizer for manager. Objective is to maximize the profit or to maximize the rating (restaurant.get_total_rating())
-    return sum_real_waiting_time - sum_ideal_waiting_time
-
-
-if __name__ == '__main__':
-    # Create model that solves optimization problems
-    opt_model = highs.Model()
 
     # Create the Mesa Model
     restaurant = RestaurantModel()
 
-    # Define the variables (add 1 to the upper bound to include the upper bound)
-    service_agents = list(range(1, 20 + 1))  # There cannot be more than 20 service agents
+    for i in x:
+        # Set the number of service agents and parallel preparation according to the optimization
+        Config().service.service_agents = int(i)
 
+        # Run the model with the updated configuration
+        while restaurant.running and restaurant.steps < Config().run.step_amount:
+            restaurant.step()
+            wait_time, profit = restaurant.evaluate()
+
+    return (wait_time, profit)
+
+
+if __name__ == "__main__":
+    # Create model that solves optimization problems
+    opt_model = highs.Model()
+
+    SIMULATION_STEPS = 50
+    x0 = np.ones(SIMULATION_STEPS) * 5  # Start mit 5 Agenten pro Zeitschritt
+    lb = np.ones(SIMULATION_STEPS) * 1  # Mindestens 1 Agent
+    ub = np.ones(SIMULATION_STEPS) * 20  # Maximal 20 Agenten
+
+    total_waiting_time = 0
+    total_profit = 0
 
     # Add the variables to the model
-    x = opt_model.add_variables(service_agents,domain=poi.VariableDomain.Continuous, lb=1)
+    for i in range(SIMULATION_STEPS):
+        x = opt_model.add_variable(domain=poi.VariableDomain.Continuous,
+                                   lb=int(lb[i]),
+                                   ub=int(ub[i]),
+                                   name=f"x{i}")
 
-    # Get a list of all possible permutations of the variables for the loop
-    # permutations = list(product(service_agents, parallel_preparation, max_customers_per_agent))
+    # Minimize the total waiting time
+    opt_model.set_objective(objective_function(x0)[0], poi.ObjectiveSense.Minimize)
+    opt_model.optimize()
 
-    # Variable to store the best objective value and corresponding parameters
-    best_obj_value = float('inf')
-    best_params = None
+    # Get the optimal solution
+    total_waiting_time = opt_model.get_obj_value()
 
-    # Iterative optimization over all permutations
-    for sa in service_agents:
 
-        # Set the objective function
-        opt_model.set_objective(model_run(restaurant, sa), poi.ObjectiveSense.Minimize)
+    # Maximize the profit
+    opt_model.set_objective(objective_function(x0)[1], poi.ObjectiveSense.Maximize)
+    opt_model.optimize()
 
-        # Optimize the model
-        opt_model.optimize()
+    # Get the optimal solution
+    total_profit = opt_model.get_obj_value()
 
-        # Check if the current objective value is better than the best one found so far
-        if opt_model.get_obj_value() < best_obj_value:
-            best_obj_value = opt_model.get_obj_value()
-            best_params = sa
+    print(f"Total waiting time: {total_waiting_time}, Total profit: {total_profit}")
 
-        # Print the results
-        print(f"\t({sa}) -> Costs: {opt_model.get_obj_value()}")
-
-    """
-    # Print the results
-    print(f"Total costs: {opt_model.get_obj_value()}")
-
-    for p in permutations:
-        print(f"{p}: {opt_model.get_variable_attribute(x[p], poi.VariableAttribute.Value)}")
-    """
-
-    # Output the best objective value and corresponding parameters
-    print(f"Best objective value: {best_obj_value}")
-    print(
-        f"Best parameters: Service Agents = {best_params}")
-
-    # TODO: Was macht der Optimizer gerade überhaupt?! Optimizer liefert aktuell immer als Wert 1. Service Agents wird zudem dynamisch über den Manager angepasst
+    # Get the optimal solution
+    # x_opt = [x.get_value() for x in opt_model.get_variables()]
